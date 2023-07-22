19443a25e70bddd73158f44b2c868e72ebbe3a31
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2023-07-23 03:27:45 +0700

    Add files via upload

--- 404/index.php
+++ 404/index.php
@@ -1,1 +1,65 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Page Not Found!</title>
+  <link rel="stylesheet" href="./style.css">
 
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<!-- end about -->
+
+    <section class="wrapper">
+
+        <div class="container">
+
+            <div id="scene" class="scene" data-hover-only="false">
+
+
+                <div class="circle" data-depth="1.2"></div>
+
+                <div class="one" data-depth="0.9">
+                    <div class="content">
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                    </div>
+                </div>
+
+                <div class="two" data-depth="0.60">
+                    <div class="content">
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                    </div>
+                </div>
+
+                <div class="three" data-depth="0.40">
+                    <div class="content">
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                    </div>
+                </div>
+
+                <p class="p404" data-depth="0.50">404</p>
+                <p class="p404" data-depth="0.10">404</p>
+
+            </div>
+
+            <div class="text">
+                <article>
+                    <p>Không tìm thấy trang này!<br>Bấm vào nút bên dưới để quay lại trang chủ</p>
+                    <button onclick="window.location.href='/'">Home</button>
+                </article>
+            </div>
+
+        </div>
+    </section>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
---
+++ 404/script.js
@@ -0,0 +1,6 @@
+// Parallax Code
+        var scene = document.getElementById('scene');
+        var parallax = new Parallax(scene);
+        async function redirect(){
+                window.location.href("https://google.com/")
+        }
